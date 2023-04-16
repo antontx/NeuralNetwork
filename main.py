1290e185581cb9bac663811fcad375a21a1483ff
antontx <tarisjan@gmail.com>
2023-04-16 12:55:56 +0200

    removed unzipping the data - now unecessary because gd can handle the tuples

--- main.py
+++ main.py
@@ -7,15 +7,10 @@ np.random.seed(1)
 
 training_data, test_data = load("MNIST_dataset")
 
-training_features, training_labels = zip(*training_data)
-test_features, test_labels = zip(*test_data)
-
-
 mlp = dnn.DenseNeuralNetwork([784, 32, 16, 10])
 
 print("training")
-mlp.train_mini_batch(training_data,
-                     epochs=10000, batch_size=10, learning_rate=1, test_data=test_data)
+mlp.train_mini_batch(training_data,epochs=30, batch_size=10, learning_rate=1, test_data=test_data)
 
 
 print("done!")
