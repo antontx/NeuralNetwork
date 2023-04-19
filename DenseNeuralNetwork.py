c8878c77d634b62adf91d855fa94598523e70661
antontx <tarisjan@gmail.com>
2023-04-19 23:41:39 +0200

    plotted accuracy vs cost & mnist number

--- DenseNeuralNetwork.py
+++ DenseNeuralNetwork.py
@@ -78,6 +78,30 @@ class DenseNeuralNetwork():
                 out[0].append(np.mean(self.MeanSquaredError(test_data)))
                 out[1].append(self.evaluate(test_data))
 
+        return out
+    
+    def train_plot2(self, training_data, gd = "mini-batch",batch_size= 32, learning_rate=0.1, epochs=250, test_data=None, interval = None):
+        if interval == None:
+            interval = epochs // 100 if epochs >= 100 else 5
+
+        out = [self.evaluate(test_data)]
+
+        for epoch in range(epochs):
+            if gd == "mini-batch":
+                self.train_mini_batch(training_data, batch_size, learning_rate, epochs, test_data)
+            elif gd == "batch":
+                self.train_batch(training_data, learning_rate, epochs, test_data)
+            elif gd == "sgd":
+                self.train_sgd(training_data, learning_rate, epochs, test_data)
+            else:
+                raise Exception("Invalid Gradient Descent Method")
+
+            if epoch % interval == 0 or epoch == epochs - 1:
+                print(f"Epoch {epoch}: {self.evaluate(test_data)} MSE: {np.mean(self.MeanSquaredError(test_data))}")
+                out.append(self.evaluate(test_data))
+
+        return out
+
     def train_sgd(self, training_data, learning_rate=None, epochs=None, test_data=None):
         for x, y in training_data:  # for every example in the data set
                 bias_gradients, weight_gradients = self.backprop(x, y)  # calculate the gradient
--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import DenseNeuralNetwork as dnn
 from MNISTDataLoader import load
 np.random.seed(1)
 
-
 training_data, test_data = load("MNIST_dataset")
 
 mlp = dnn.DenseNeuralNetwork([784, 32, 16, 10])
-
-print("training")
 mlp.train(training_data=training_data, epochs=20, batch_size=32, learning_rate=0.1, test_data=test_data, gd="mini-batch")
 
-
-print("done!")
